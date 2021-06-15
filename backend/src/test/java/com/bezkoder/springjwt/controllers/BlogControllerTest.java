d0388cb19d5ac9db098af800c55a32c423950658
Petrov,Aleks A.P <aleks.petrov@student.fontys.nl>
2021-06-15 17:16:41 +0200

    ci/cd pipeline

---
+++ backend/src/test/java/com/bezkoder/springjwt/controllers/BlogControllerTest.java
@@ -0,0 +1,28 @@
+package com.bezkoder.springjwt.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlogControllerTest {
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
